99799d55013787974a3a9982239e89d5532fefe4
Xu Guohong <xgh127@sjtu.edu.cn>
2024-06-22 11:31:16 +0800

    放弃边变细

--- src/components/NeoGraph.js
+++ src/components/NeoGraph.js
@@ -3,7 +3,7 @@ import Neovis from "neovis.js/dist/neovis.js";
 import { Card, Layout } from "antd";
 import InfoDisplay from "./InfoDisplay";
 import { SubGraph } from "./SubGraph";
-import { allRelationships, labels } from "./Constant";
+import { labels } from "./Constant";
 
 const { Meta } = Card;
 
@@ -20,17 +20,7 @@ export function generateLabelConfigs() {
   });
   return config;
 }
-export function generateRelationshipConfigs() {
-  const baseRelationshipConfig = {
-    thickness: 0.1, // 设置所有关系的厚度为0.1
-  };
-  const relationshipConfig = {};
-  allRelationships.forEach(relationship => {
-    relationshipConfig[relationship] = { ...baseRelationshipConfig };
-  });
 
-  return relationshipConfig;
-}
 const NeoGraph = props => {
   //定义一个NeoGraph组件，接收props参数
   const {
@@ -54,7 +44,13 @@ const NeoGraph = props => {
       server_password: neo4jPassword,
       //caption属性换成label属性即可
       labels: generateLabelConfigs(),
-      relationships: generateRelationshipConfigs(),
+      // relationships: {
+      //   ...generateRelationshipConfigs(),
+      //     "公司":{
+      //       thickness: 0.1,
+      //
+      //     }
+      // },
       arrows: true,
       initial_cypher: cypherQuery,
     };
