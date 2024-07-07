c4a67ef58f36870a18220e67781a40c2bcc42954
Xu Guohong <xgh127@sjtu.edu.cn>
2024-07-07 20:12:54 +0800

    Updates

---
+++ precache-manifest.dbd3deda56ef83347449b03a7641c2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab25c2916ec1907e1b02644e13d3b4c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5f7301339b865f7c0cd",
+    "url": "./static/css/main.6d6734d2.chunk.css"
+  },
+  {
+    "revision": "58f009bf2487a7240523",
+    "url": "./static/js/2.d938375a.chunk.js"
+  },
+  {
+    "revision": "fb5eacee9caf0a79c2fa633e4d415fec",
+    "url": "./static/js/2.d938375a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f7301339b865f7c0cd",
+    "url": "./static/js/main.b38ecfa7.chunk.js"
+  },
+  {
+    "revision": "8efa8edee73c9e693e08",
+    "url": "./static/js/runtime-main.64e4b93c.js"
+  }
+]);
